ab7db2dd3b00a10ba53ae561913426866dc21b20
meshpig <matt@good-words.com.au>
2023-05-07 08:14:20 +1000

    Initial Commit

---
+++ FFT Water/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FFT Water
+//
+//  Created by Melinda Loew on 7/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FFT Water/FFT_WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FFT_WaterApp.swift
+//  FFT Water
+//
+//  Created by Melinda Loew on 7/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct FFT_WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
